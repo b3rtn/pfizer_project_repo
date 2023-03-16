f9d9acbbb01017bf5539b4d3b28e8e1dffd1fd31
b3rtn <106857050+b3rtn@users.noreply.github.com>
2023-03-16 16:52:03 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,4 +10,4 @@ subject = 'WHOIS Results Update'
 body = """
 The WHOIS results have been updated. Please see the attached JSON file for details.
 """
-DOMAINS = ["www.disorderstatus.ru","www.hngleg.online","www.pionsecar.com","www.garatinterbk.online","www.buildarat.com","www.cryptotradexinvest.com"]
+DOMAINS = []
